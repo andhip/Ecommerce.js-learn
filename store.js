ab8fa775a2f6f51fd6168c857261fde3da9bbe31
andhip <andhi.puspianto10@gmail.com>
2021-03-11 21:29:01 +0700

    update js. add to cart mtd

--- store.js
+++ store.js
@@ -29,5 +29,26 @@ function addToCartClicked(evenet){
 function addItemTocart(title, price, imageSrc){
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
-    var cartItems  = document.getElementsByClassName('cart-items')
+    var cartItems  = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for(var i=0; 1 < cartItemNames.length; i++){
+        if(cartItemNames[i].innerHTML == title){
+            alert('product successfully added to cart')
+            return
+        }
+    }
+    var cartRowContents = `<div class="cart-item cart-column">
+                                <img class="cart-item-image src="${imageSrc}" widht="150" height="150">
+                                <span class="cart-item-title">${title}</span>
+                            </div>
+                                 <span class="cart-price cart-column">${price}</span>
+                                    <div class="cart-qty cart-column">
+                                        <input class="cart-qty-input type="nunmber" value="1">
+                                        <button class="btn btn-danger" type="button">REMOVE</button>
+                                    </div>`
+                            cartRow.innerHTML   = cartRowContents
+                            cartItems.append(cartRow)
+                            cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCart)
+                            cartRow.getElementsByClassName('cart-qty-input')[0].addEventListener('change', qtyChange)
+
 }
